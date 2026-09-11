58259978147e635b690dfb1900788f35d5ffa75a
ciskk <francisco.pontes00@aluno.ifce.edu.br>
2026-09-11 14:14:23 -0300

    mais xp ao derrotar boss

--- src/config/enemies.js
+++ src/config/enemies.js
@@ -12,9 +12,9 @@ export const ENEMY_TYPES ={
 };
 
 export const BOSS_TYPES = {
-  1: { name: 'Lorde Vampírico', hp: 8000, radius: 38, speed: 1.85, color: '#8e44ad', damage: 30, xp: 200, bossId: 1 },
-  2: { name: 'Monólito Abissal', hp: 40000, radius: 48, speed: 0.95, color: '#c0392b', damage: 50, xp: 530, bossId: 2 },
-  3: { name: 'Ceifador Supremo', hp: 60000, radius: 44, speed: 2.1, color: '#00cec9', damage: 70, xp: 950, bossId: 3 },
+  1: { name: 'Lorde Vampírico', hp: 8000, radius: 38, speed: 1.85, color: '#8e44ad', damage: 30, xp: 1000, bossId: 1 },
+  2: { name: 'Monólito Abissal', hp: 40000, radius: 48, speed: 0.95, color: '#c0392b', damage: 50, xp: 2000, bossId: 2 },
+  3: { name: 'Ceifador Supremo', hp: 60000, radius: 44, speed: 2.1, color: '#00cec9', damage: 70, xp: 4000, bossId: 3 },
   4: { name: 'Soberano do Abismo', hp: 120000, radius: 52, speed: 1.25, color: '#8e44ad', damage: 90, xp: 1, bossId: 4, isFinalBoss: true }
 };
 
